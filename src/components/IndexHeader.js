9683808744310e1a053b666acfcac5d6f03d1cc6
sergio1599 <sergioq15@hotmail.com>
2021-06-24 14:17:28 -0500

    delete some files

---
+++ src/components/IndexHeader.js
@@ -0,0 +1,10 @@
+
+function IndexHeader() {
+    return (
+        <div className="IndexHeader">
+            <h1>Notas UPTC</h1>
+        </div>
+    );
+}
+
+export default IndexHeader;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import IndexHeader from "./components/IndexHeader";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <IndexHeader/>,
   document.getElementById('root')
 );
 
