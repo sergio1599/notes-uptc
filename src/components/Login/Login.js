a25a412599464e8c2c9eaf66a13341dfb70495c4
sergio1599 <sergioq15@hotmail.com>
2021-08-26 14:49:17 -0500

    adding bootstrap and create a form for the login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,44 @@
-import styles from './login.module.css'
-import React from 'react'
-
-
+import styles from "./login.module.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button } from "react-bootstrap";
 
 export const Login = () => {
-    return (
-        <body>
-            <div className={styles.container}>
-                <div className={styles.image}>
-
-                </div>
-                <div className={styles.login}>
-                    <h1>Notas UPTC</h1>
-                </div>
+  return (
+    <body>
+      <div className={"container"}>
+        <div className={"row justify-content-center"}>
+          <div className={"col-4"}>
+            <div className={"text-center"}>
+              <img
+                src="./images/logo-recortado.png"
+                width="300"
+                class="rounded"
+                alt="..."
+              />
             </div>
+          </div>
+          <div className={"col-4"}>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Correo</Form.Label>
+                <Form.Control type="email" placeholder="Ingresa correo" />
+              </Form.Group>
 
-        </body>
-    )
-}
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" placeholder="Contraseña" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Recordarme" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Enviar
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
