d0e12bb708416026a51c3cfe286c163180667061
sergio1599 <sergioq15@hotmail.com>
2021-08-26 15:32:29 -0500

    branch created for test login page whitout bootstrap

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,44 +1,11 @@
 import styles from "./login.module.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Form, Button } from "react-bootstrap";
+
 
 export const Login = () => {
   return (
-    <body>
-      <div className={"container"}>
-        <div className={"row justify-content-center"}>
-          <div className={"col-4"}>
-            <div className={"text-center"}>
-              <img
-                src="./images/logo-recortado.png"
-                width="300"
-                class="rounded"
-                alt="..."
-              />
-            </div>
-          </div>
-          <div className={"col-4"}>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Correo</Form.Label>
-                <Form.Control type="email" placeholder="Ingresa correo" />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Recordarme" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Enviar
-              </Button>
-            </Form>
-          </div>
+        <div >
+            Titulo
         </div>
-      </div>
-    </body>
   );
 };
