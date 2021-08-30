9d072ae4c0aaef4234652dd91483be9a4a47ab21
sergio1599 <sergioq15@hotmail.com>
2021-08-30 18:41:49 -0500

    adding icons to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from './footer.module.css';
+import Icon from '@mdi/react';
+import {mdiGithub,mdiLinkedin, mdiInstagram} from '@mdi/js';
+
+const FooterCards = (props) => {
+    return (<Icon path={props.path}
+                  size={props.size}
+                  color={props.color}
+    />);
+}
 
 
 export const Footer = () => {
+    
+    let gitHub = <FooterCards path={mdiGithub} size={1.8} color={"#ffff"}/>
+    let instagram = <FooterCards path={mdiInstagram} size={1.8} color={"#ffff"}/>
+    let linkedin = <FooterCards path={mdiLinkedin} size={1.8} color={"#ffff"}/>
+
     return (
         <div className={styles.footer}>
              <footer>
@@ -10,9 +24,15 @@ export const Footer = () => {
                     <div className={styles.elements}>
                         <div>
                             <p>Ruben Macías</p>
+                            <a title={'GitHub'} href="https://github.com/rubenmacias2">{gitHub}</a>
+                            <a title={'LinkedIn'} href="https://www.linkedin.com/in/ruben-macias-socha-205111205/">{linkedin}</a>
+                            <a title={'Instagram'} href="https://www.instagram.com/rubenmaciash/">{instagram}</a>
                         </div>
                         <div>
                             <p>Sergio Quintana</p>
+                            <a title={'GitHub'} href="https://github.com/sergio1599">{gitHub}</a>
+                            <a title={'LinkedIn'} href="https://www.linkedin.com/in/sergio1599/">{linkedin}</a>
+                            <a title={'Instagram'} href="https://www.instagram.com/s_alejandro_15/">{instagram}</a> 
                         </div>
                     </div>
                 </div>
