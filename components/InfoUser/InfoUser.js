06c2ee8ea8537e71e88bac355a51ad5654fa9cc0
sergio1599 <sergioq15@hotmail.com>
2021-09-13 10:01:45 -0500

    adding elements to sidebar menu

--- components/InfoUser/InfoUser.js
+++ components/InfoUser/InfoUser.js
@@ -4,7 +4,13 @@ import styles from './infoUser.module.css'
 const InfoUser = () => {
     return (
         <div className={styles.info}>
-            <div>Hola</div>
+            <div className={styles.sideBar}>
+                  <img className={styles.profilePic} src={"/images/profile.jpg"}/>
+                  <button className={styles.buttons}>Notas</button>
+                  <button className={styles.buttons}>Archivos</button>
+                  <button className={styles.buttons}>Favoritos</button>
+                  <button className={styles.confBtn}>Configuración</button>
+            </div>
             <div>Hola</div>
         </div>
     )
