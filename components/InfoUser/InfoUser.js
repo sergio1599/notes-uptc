bd59c5819444baae79b6ece7f4a4906a9c0fc628
sergio1599 <sergioq15@hotmail.com>
2021-09-13 09:17:17 -0500

    adding component userInfo

---
+++ components/InfoUser/InfoUser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './infoUser.module.css'
+
+const InfoUser = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InfoUser
