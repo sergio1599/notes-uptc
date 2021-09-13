51a8692fedc5b5590e1622e8fb9cfa8e48713462
sergio1599 <sergioq15@hotmail.com>
2021-09-13 15:41:39 -0500

    updating hover in the sidebar user buttons

--- components/InfoUser/InfoUser.js
+++ components/InfoUser/InfoUser.js
@@ -6,22 +6,13 @@ const InfoUser = () => {
         <div className={styles.info}>
             <div className={styles.sideBar}>
                   <img className={styles.profilePic} src={"/images/profile.jpg"}/>
-                  <a>
-                    <button className={styles.buttons}>Notas</button>
-                  </a>
-                  <a>
-                    <button className={styles.buttons}>Archivos</button>
-                  </a>
-                  <a>
-                    <button className={styles.buttons}>Favoritos</button>
-                  </a>
+                  <button className={styles.buttons}>Notas</button>
+                  <button className={styles.buttons}>Archivos</button>
+                  <button className={styles.buttons}>Favoritos</button>
                   <div className={styles.contImage}>
                     <img  src={"/images/logo-recortado.png"}/>
                   </div>
-                  <a>
-                    <button className={styles.confBtn}>Configuración</button>
-                  </a>
-                  
+                  <button className={styles.confBtn}>Configuración</button>
             </div>
             <div>Hola</div>
         </div>
