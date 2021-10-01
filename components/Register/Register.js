511659b9fb5ed58ad9656f07ea9bae6748cac571
sergio1599 <sergioq15@hotmail.com>
2021-10-01 18:39:15 -0500

    add component register

---
+++ components/Register/Register.js
@@ -0,0 +1,26 @@
+import styles from "../Register/register.module.css";
+import Link from "next/link";
+
+const Register = ({}) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Notas UPTC</h1>
+            <label>Nombre</label>
+            <input placeholder={"Escriba su nombre"} className={styles.input}/>
+            <label>Apellido</label>
+            <input placeholder={"Escriba su apellido"} className={styles.input}/>
+            <label>Contraseña</label>
+            <input placeholder={"Escriba la contraseña"} className={styles.input}/>
+            <label>Correo</label>
+            <input placeholder={"Escriba su correo"} className={styles.input}/>
+            <label>¿Que tipo de usuario vas a ser?</label>
+            <select className={styles.select}>
+                <option>Usuario</option>
+                <option>Administrador</option>
+            </select>
+
+        </div>
+    );
+};
+
+export default Register;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Login from '../components/Login/Login'
 import Footer from '../components/Footer/Footer'
+import Register from "../components/Register/Register";
 
 export default function Home() {
   return (<>
     <title>Notas UPTC</title>
     <link rel="icon" href="../public/icons/keep.ico"/>
     <div>
-        <Login/>
+        <Register/>
         <Footer/>
     </div>
   </>);
