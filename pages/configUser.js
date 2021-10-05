54cf6e2c9b6533aac194a5e4ce056bb3834558bb
sergio1599 <sergioq15@hotmail.com>
2021-10-05 13:08:21 -0500

    fix navbar error in the vercel building

--- pages/configUser.js
+++ pages/configUser.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavBar from '../../components/NavBar/NavBar'
-import Config from '../../components/Config/Config'
+import NavBar from '../components/NavBar/NavBar'
+import Config from '../components/Config/Config'
 
 export const configUser = () => {
     return (
